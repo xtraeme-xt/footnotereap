993b439554bf3003575413e27f0d9a9a6e0ce440
xtraeme.xt@gmail.com <xtraeme.xt@gmail.com@c24505a3-c3b7-b32f-c509-333312e6991f>
2012-11-17 01:33:01 +0000

    fixes issue #12, Write tool to dump registry into log (before and after)

--- autoit/scripts/footnotedbg.bat
+++ autoit/scripts/footnotedbg.bat
@@ -2,5 +2,10 @@
 cscript ".\scripts\EnableDebugMode.js" > NUL
 set LOGFILE="log\%DATE:/=-%_%TIME::=.%_footnotereap_log.txt" 
 mkdir log 2> NUL
-cscript ".\scripts\InternetExplorerVersion.js" > %LOGFILE% 
+cscript ".\scripts\InternetExplorerVersion.js" >> %LOGFILE% 
+echo. >> %LOGFILE% 
+reg query HKEY_CURRENT_USER\Software\FoonoteReaper >> %LOGFILE% 
+echo. >> %LOGFILE% 
 footnote.exe | tee -a %LOGFILE%
+echo. >> %LOGFILE% 
+reg query HKEY_CURRENT_USER\Software\FoonoteReaper >> %LOGFILE% 
